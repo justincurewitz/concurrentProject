8df384a962e18b3c3579d8bc4d3ec1ca4d4c9c66
Kristian <kristianwang@utexas.edu>
2017-04-22 12:12:39 -0500

    cause i need it

--- EE360PChatroom/app/src/main/java/com/example/ee360p/ee360pchatroom/LoginActivity.java
+++ EE360PChatroom/app/src/main/java/com/example/ee360p/ee360pchatroom/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.ee360p.ee360pchatroom;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,8 +334,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
                 finish();
             } else {
+                Intent registerIntent = new Intent(LoginActivity.this,MainActivity.class);
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
             }
