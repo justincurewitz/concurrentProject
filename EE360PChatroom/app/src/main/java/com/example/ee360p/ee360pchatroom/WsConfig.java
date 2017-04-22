03168b0a7976d20943a9a85f784ae3d1ca2ec8f7
Kristian <kristianwang@utexas.edu>
2017-04-22 12:52:57 -0500

    more tutorial stuff

---
+++ EE360PChatroom/app/src/main/java/com/example/ee360p/ee360pchatroom/WsConfig.java
@@ -0,0 +1,10 @@
+package com.example.ee360p.ee360pchatroom;
+
+/**
+ * Created by anon on 4/22/17.
+ */
+
+public class WsConfig {
+    public static final String URL_WEBSOCKET = "ws://192.168.0.102:8080/WebMobileGroupChatServer/chat?name=";
+
+}
